d534e65ba6d86f3bd424f1b8a50259c906d71b03
Maximilian Wombacher <m.wombacher@hotmail.com>
2014-05-07 11:49:10 +0200

    Precrash started

--- CApp_OnLoop.cpp
+++ CApp_OnLoop.cpp
@@ -8,7 +8,20 @@ void Capp::OnLoop() {
     Stock[0]->fly(Cap_next1);
     Stock[1]->fly(Cap_next2);
 
-    printf("crash:%d \n", Stock[0]->crash(Stock));
+  /*
+
+    if(Stock[0]->crash(Stock)==1)
+        {
+        printf("Achtung kollision");
+        }
+
+
+
+    }*/
+    if (Stock[0]->precrash(Stock,Cap_next1, Cap_next2))
+        {
+            printf("Attention collision inc.");
+        }
     if(Stock[0]->crash(Stock)==1)
     {
         Menu=Spielendcard;
