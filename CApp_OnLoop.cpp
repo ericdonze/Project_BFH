b36b91ec6236aa35eda681707c0f39e00edb08b4
Adrian Senn <adu.senn@gmail.com>
2014-05-07 11:50:53 +0200

    Update

--- CApp_OnLoop.cpp
+++ CApp_OnLoop.cpp
@@ -2,13 +2,29 @@
 
 void Capp::OnLoop() {
 
-
-
-
-    Stock[0]->fly(Cap_next1);
+   /* for(i=0;i<2;i++)
+    {
+            if(Stock[i]->get_On_click()==true)
+            {
+                if(winkel!=Cap_next1)
+                {
+                    Cap_next1=winkel;
+                    Stock[i]->fly(Cap_next1);
+                }
+                else
+                {
+                    Stock[i]->fly(Cap_next1);
+                }
+            }
+            else
+            {
+                Stock[i]->fly(Cap_next1);
+            }
+    } */
+    Stock[0]->fly(winkel);
     Stock[1]->fly(Cap_next2);
 
-    printf("crash:%d \n", Stock[0]->crash(Stock));
+
     if(Stock[0]->crash(Stock)==1)
     {
         Menu=Spielendcard;
